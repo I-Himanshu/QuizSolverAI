f443aafc086177fe7c15f66f298899835cb9d18e
Himanshu kumar <83366556+I-Himanshu@users.noreply.github.com>
2024-04-19 21:50:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -45,6 +45,8 @@ app.post('/question', async (req, res) => {
     }
 });
 
-app.listen(port, () => {
-    console.log('Server is running on port ', port);
-});
+// app.listen(port, () => {
+//     console.log('Server is running on port ', port);
+// });
+
+module.exports = app;
